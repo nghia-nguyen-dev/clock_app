cd0ba01b5516cc9c07ad2f876075197c94a60c38
Nghia <hello.nghia22@gmail.com>
2021-02-19 14:48:20 -0500

    feat: Add options object and helper functions to clock component

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,16 +1,28 @@
 import React from "react";
 
+function rmAMandPM(time) {
+	return time.replace(" PM", "").replace(" AM", "");
+}
+
+const options = {
+	hour: "2-digit",
+	minute: "2-digit",
+};
+
 class Clock extends React.Component {
-    options = {
-        hour: "2-digit",
-        minute: "2-digit",
-    }
 	state = {
-		time: new Date().toLocaleTimeString([], this.options),
+		time: new Date().toLocaleTimeString([], options),
+		afternoon: null,
 	};
 
+	componentDidMount() {
+		setInterval(() => {
+			this.setState({ time: new Date().toLocaleTimeString([], options) });
+		}, 1000);
+	}
+
 	render() {
-		return <div className="clock">{this.state.time}</div>;
+		return <div className="clock">{rmAMandPM(this.state.time)}</div>;
 	}
 }
 
