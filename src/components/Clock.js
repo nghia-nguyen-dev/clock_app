bc3ec0b0206191507cd548435827fafcf430811e
Nghia <hello.nghia22@gmail.com>
2021-02-19 16:35:01 -0500

    refactor: Clean up Clock component

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,28 +1,44 @@
 import React from "react";
 
-function rmAMandPM(time) {
-	return time.replace(" PM", "").replace(" AM", "");
-}
-
 const options = {
 	hour: "2-digit",
 	minute: "2-digit",
+	timeZoneName: "short",
 };
 
 class Clock extends React.Component {
 	state = {
-		time: new Date().toLocaleTimeString([], options),
-		afternoon: null,
+		time: this.getTime(0),
+		timeZone: this.getTime(2),
+		timeOfDay: this.getTime(1),
 	};
 
 	componentDidMount() {
 		setInterval(() => {
-			this.setState({ time: new Date().toLocaleTimeString([], options) });
+			this.tick();
 		}, 1000);
 	}
 
+	tick() {
+		this.setState({
+			time: this.getTime(0),
+			timeZone: this.getTime(2),
+			timeOfDay: this.getTime(1),
+		});
+	}
+
+	getTime(index) {
+		return new Date().toLocaleTimeString([], options).split(" ")[index]; // [time, AM/PM, timezone]
+	}
+
 	render() {
-		return <div className="clock">{rmAMandPM(this.state.time)}</div>;
+		return (
+			<div className="clock">
+				{this.state.time}
+				{this.state.timeZone}
+				{this.state.timeOfDay}
+			</div>
+		);
 	}
 }
 
