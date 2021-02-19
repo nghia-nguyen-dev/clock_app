2c3a978c403ea632851db02d84473c6fdc82fb22
Nghia <hello.nghia22@gmail.com>
2021-02-19 12:14:56 -0500

    feat: Create Button component

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Button extends React.Component {
+
+    render() {
+        <div className="Button">
+            <p>More</p>
+        </div>
+    }
+}
+
+export default Button;
