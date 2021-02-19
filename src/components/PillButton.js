e1febd2e0140313d5cadd691f23c309b9d6c18fe
Nghia <hello.nghia22@gmail.com>
2021-02-19 12:40:42 -0500

    feat: Create CircleButton

--- src/components/PillButton.js
+++ src/components/PillButton.js
@@ -1,10 +1,13 @@
 import React from "react";
+import CircleButton from './CircleButton';
+
 
 class PillButton extends React.Component {
 	render() {
 		return (
-			<div className="Button">
+			<div className="pill-button">
 				<p>More</p>
+				<CircleButton />
 			</div>
 		);
 	}
