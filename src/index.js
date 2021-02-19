e9a43447f9ff6196e5456b7882b40dce158d6f3a
Nghia <hello.nghia22@gmail.com>
2021-02-19 12:02:20 -0500

    feat: Add css font file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './css/index.css'
+import "./css/index.css";
 
 const App = () => {
-    return (
-        <div>Hello</div>
-    )
-}
+	return <div>Hello</div>;
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
