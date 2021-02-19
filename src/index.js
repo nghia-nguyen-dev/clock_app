a815a1078011b460246a72cb3e7b6889c78f8bbf
Nghia <hello.nghia22@gmail.com>
2021-02-19 11:47:16 -0500

    feat: Flesh out index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './css/index.css'
+
+const App = () => {
+    return (
+        <div>Hello</div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
