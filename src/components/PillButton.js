3823b2310bb46c0333a1454564634b34fa4ae3ba
Nghia <hello.nghia22@gmail.com>
2021-02-19 12:35:24 -0500

    refactor: Update Button name

---
+++ src/components/PillButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class PillButton extends React.Component {
+	render() {
+		return (
+			<div className="Button">
+				<p>More</p>
+			</div>
+		);
+	}
+}
+
+export default PillButton;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PillButton from './components/PillButton';
 
 import "./css/index.css";
 
 const App = () => {
-	return <div>Hello</div>;
+	return (
+        <div>
+            <PillButton />
+        </div>
+    )
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
