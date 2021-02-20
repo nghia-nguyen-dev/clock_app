f35d7b1cb1b5aabe1643ab194ed05eec0b50a1c8
Nghia <hello.nghia22@gmail.com>
2021-02-19 22:38:26 -0500

    feat: Add quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Quote = () => {
+    return (
+        <div>random quote</div>
+    )
+}
+
+export default Quote;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PillButton from './components/PillButton';
 import Widget from './components/Widget'
+import PillButton from './components/PillButton';
+import Quote from './components/Quote'
 
 import "./css/index.css";
 
 const App = () => {
 	return (
         <div className="app">
-            <PillButton text="more"/>
+            <Quote />
             <Widget />
         </div>
     )
