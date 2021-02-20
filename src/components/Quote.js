57782bf16b2c12f36023e83b0486fc1eed2d7321
Nghia <hello.nghia22@gmail.com>
2021-02-19 23:35:12 -0500

    refactor: Convert function component into class component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const Quote = () => {
-    return (
-        <div>random quote</div>
-    )
+class Quote extends React.Component {
+	state = {
+		author: null,
+		content: null,
+	};
+
+	componentDidMount() {
+		fetch("https://api.quotable.io/random")
+			.then((res) => res.json())
+			.then((data) => {
+				this.setState({ author: data.author, content: data.content });
+			})
+			.catch((err) => console.log(err));
+	}
+
+	render() {
+		return (
+			<div className="quote">
+				{this.state.content}
+				{this.state.author}
+			</div>
+		);
+	}
 }
 
 export default Quote;
