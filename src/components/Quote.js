d0ad9266fad13538c2b1c1b12e1e3de2566bb78f
Nghia <hello.nghia22@gmail.com>
2021-02-21 18:13:45 -0500

    refactor: Rm constructor and change getQuote to arrow func in Quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,28 +3,16 @@ import "../css/Quote.css";
 import Refresh from "../components/icons/Refresh";
 
 class Quote extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			author: null,
-			content: null,
-		};
-		this.getQuote = this.getQuote.bind(this)
-	}
+	state = {
+		author: null,
+		content: null,
+	};
 
 	componentDidMount() {
-		fetch("https://api.quotable.io/random")
-			.then((res) => res.json())
-			.then((data) => {
-				this.setState({
-					author: data.author,
-					content: data.content,
-				});
-			})
-			.catch((err) => console.log(err));
+		this.getQuote();
 	}
 
-	getQuote() {
+	getQuote = () => {
 		fetch("https://api.quotable.io/random")
 			.then((res) => res.json())
 			.then((data) => {
@@ -34,14 +22,17 @@ class Quote extends React.Component {
 				});
 			})
 			.catch((err) => console.log(err));
-	}
+	};
 
 	render() {
 		return (
 			<div className="quote">
 				<div className="quote__text">
 					<p className="quote__content">{this.state.content}</p>
-					<p className="quote__author"><span>—</span>{this.state.author}</p>
+					<p className="quote__author">
+						<span>—</span>
+						{this.state.author}
+					</p>
 				</div>
 				<Refresh
 					className="quote__refresh-icon"
