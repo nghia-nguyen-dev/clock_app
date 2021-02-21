e0b3e4d8e049cd0ca9fef8dec8a8af63b8a3cf38
Nghia <hello.nghia22@gmail.com>
2021-02-20 19:17:25 -0500

    feat: Add logic to get backdrop image

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -6,10 +6,18 @@ const image = {
     night:'backdrop__image--night',
 }
 
+function getGreeting(hour) {
+	if (hour < 12) {
+		return image.day;
+	} else {
+		return image.night;
+	} 
+}
+
 const Backdrop = () => {
 	return (
 		<div className="backdrop">
-			<div className={`.backdrop__image ${image.day}`} />
+			<div className={`backdrop__image ${getGreeting(new Date().getHours())}`} />
 			<div className="backdrop__opacity-layer" />
 		</div>
 	);
