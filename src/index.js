9a35d6b9ceaea66a84fc6fe48ca0ee5bb4122abd
Nghia <hello.nghia22@gmail.com>
2021-02-21 13:32:36 -0500

    feat: Add styling and re-structure html

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Widget from './components/Widget'
-import PillButton from './components/PillButton';
-import Quote from './components/Quote'
-import Backdrop from './components/Backdrop'
-import Dashboard from './components/Dashboard'
+import Widget from "./components/Widget";
+import PillButton from "./components/PillButton";
+import Quote from "./components/Quote";
+import Backdrop from "./components/Backdrop";
+import Dashboard from "./components/Dashboard";
 
 import "./css/index.css";
+import Clock from "./components/Clock";
 
 const App = () => {
 	return (
-        <div className="app">
-            <Backdrop />
-            <Quote />
-            <Widget />
-            {/* <PillButton text="more"/> */}
-            <Dashboard />
-        </div>
-    )
+		<div className="app">
+			<Backdrop />
+			<div className="container">
+				<div className="widgets">
+					<Quote />
+					<div>
+						<Widget />
+						<PillButton text="more" />
+					</div>
+				</div>
+				<Dashboard />
+			</div>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
