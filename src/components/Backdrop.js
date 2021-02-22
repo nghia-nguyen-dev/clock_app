2515d446a56aceb2aab3533fc27e397aba515b4f
Nghia <hello.nghia22@gmail.com>
2021-02-21 21:54:56 -0500

    feat: Add new images and logic to dashboard

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -1,26 +1,33 @@
 import React from "react";
-import '../css/Backdrop.css'
+import "../css/Backdrop.css";
 
 const image = {
-    day: 'backdrop__image--day',
-    night:'backdrop__image--night',
-}
+	day: "backdrop__image--day",
+	afternoon: "backdrop__image--afternoon",
+	night: "backdrop__image--night",
+};
 
 function getGreeting(hour) {
 	if (hour < 12) {
 		return image.day;
+	} else if (hour < 18) {
+		return image.afternoon;
 	} else {
 		return image.night;
-	} 
+	}
 }
 
 const Backdrop = () => {
 	return (
 		<div className="backdrop">
-			<div className={`backdrop__image ${getGreeting(new Date().getHours())}`} />
+			<div
+				className={`backdrop__image ${getGreeting(
+					new Date().getHours()
+				)}`}
+			/>
 			<div className="backdrop__opacity-layer" />
 		</div>
 	);
 };
 
-export default Backdrop
+export default Backdrop;
